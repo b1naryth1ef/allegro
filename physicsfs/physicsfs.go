c579d4849570712e24e323267681b63f8e29a7b3
Coşku Baş <cosku.bas@gmail.com>
2013-03-06 21:29:58 +0200

    Phyfs implemented

---
+++ physicsfs/physicsfs.go
@@ -0,0 +1,13 @@
+package physicsfs
+// #cgo LDFLAGS: -lallegro_physfs
+// #include <allegro5/allegro.h>
+// #include <allegro5/allegro_physfs.h>
+import "C"
+
+func SetPhysfsFileInterface() {
+	C.al_set_physfs_file_interface()
+}
+
+func GetAllegroPhysfsVersion() uint32 {
+	return uint32(C.al_get_physfs_version())
+}

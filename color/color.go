77d0202168fb1bb07e3de0001e56c5f44be9ce59
Coşku Baş <cosku.bas@gmail.com>
2013-03-20 20:55:37 +0200

    Color module fixed. Color addon implemented

--- color/color.go
+++ color/color.go
@@ -5,15 +5,123 @@ package color
 // #include <allegro5/allegro_color.h>
 import "C"
 
-import "github.com/tapir/allegro"
+import (
+	"allegro"
+	"unsafe"
+)
 
-
-func ColorCmyk(c, m, y, k float32) allegro.Color {
-	return (allegro.Color)(C.al_color_cmyk(C.float(c), C.float(m), C.float(y), C.float(k)))
+func GetVersion() uint32 {
+	return uint32(C.al_get_allegro_color_version())
 }
 
-func ColorCmykToRgb(cyan, magenta, yellow, key float32) (float32, float32, float32) {
+func Cmyk(c, m, y, k float32) *allegro.Color {
+	r := C.al_color_cmyk(C.float(c), C.float(m), C.float(y), C.float(k))
+	return (*allegro.Color)(unsafe.Pointer(&r))
+}
+
+func CmykToRgb(cyan, magenta, yellow, key float32) (float32, float32, float32) {
 	var r, g, b C.float
 	C.al_color_cmyk_to_rgb(C.float(cyan), C.float(magenta), C.float(yellow), C.float(key), &r, &g, &b)
 	return float32(r), float32(g), float32(b)
+}
+
+func RgbToCmyk(r, g, b float32) (float32, float32, float32, float32) {
+	var c, m, y, k C.float
+	C.al_color_rgb_to_cmyk(C.float(r), C.float(g), C.float(b), &c, &m, &y, &k)
+	return float32(c), float32(m), float32(y), float32(k)
+}
+
+func Hsl(h, s, l float32) *allegro.Color {
+	r := C.al_color_hsl(C.float(h), C.float(s), C.float(l))
+	return (*allegro.Color)(unsafe.Pointer(&r))
+}
+
+func HslToRgb(h, s, l float32) (float32, float32, float32) {
+	var r, g, b C.float
+	C.al_color_hsl_to_rgb(C.float(h), C.float(s), C.float(l), &r, &g, &b)
+	return float32(r), float32(g), float32(b)
+}
+
+func RgbToHsl(r, g, b float32) (float32, float32, float32) {
+	var h, s, l C.float
+	C.al_color_rgb_to_hsl(C.float(r), C.float(g), C.float(b), &h, &s, &l)
+	return float32(h), float32(s), float32(l)
+}
+
+func Hsv(h, s, v float32) *allegro.Color {
+	r := C.al_color_hsv(C.float(h), C.float(s), C.float(v))
+	return (*allegro.Color)(unsafe.Pointer(&r))
+}
+
+func HsvToRgb(h, s, v float32) (float32, float32, float32) {
+	var r, g, b C.float
+	C.al_color_hsv_to_rgb(C.float(h), C.float(s), C.float(v), &r, &g, &b)
+	return float32(r), float32(g), float32(b)
+}
+
+func RgbToHsv(r, g, b float32) (float32, float32, float32) {
+	var h, s, v C.float
+	C.al_color_rgb_to_hsv(C.float(r), C.float(g), C.float(b), &h, &s, &v)
+	return float32(h), float32(s), float32(v)
+}
+
+func Html(s string) *allegro.Color {
+	hs := C.CString(s)
+	defer C.free(unsafe.Pointer(hs))
+	r := C.al_color_html(hs)
+	return (*allegro.Color)(unsafe.Pointer(&r))
+}
+
+func HtmlToRgb(s string) (float32, float32, float32) {
+	var r, g, b C.float
+	hs := C.CString(s)
+	defer C.free(unsafe.Pointer(hs))
+	C.al_color_html_to_rgb(hs, &r, &g, &b)
+	return float32(r), float32(g), float32(b)
+}
+
+func RgbToHtml(r, g, b float32) string {
+	var cstr *C.char
+	defer C.free(unsafe.Pointer(cstr))
+	C.al_color_rgb_to_html(C.float(r), C.float(g), C.float(b), cstr)
+	return C.GoString(cstr)
+}
+
+func Name(name string) *allegro.Color {
+	s := C.CString(name)
+	defer C.free(unsafe.Pointer(s))
+	r := C.al_color_name(s)
+	return (*allegro.Color)(unsafe.Pointer(&r))
+}
+
+func NameToRgb(name string) (float32, float32, float32) {
+	var r, g, b C.float
+	hs := C.CString(name)
+	defer C.free(unsafe.Pointer(hs))
+	C.al_color_name_to_rgb(hs, &r, &g, &b)
+	return float32(r), float32(g), float32(b)
+}
+
+func RgbToName(r, g, b float32) string {
+	var cstr *C.char
+	defer C.free(unsafe.Pointer(cstr))
+	C.al_color_rgb_to_html(C.float(r), C.float(g), C.float(b), cstr)
+	return C.GoString(cstr)
+}
+
+func Yuv(y, u, v float32) *allegro.Color {
+	r := C.al_color_yuv(C.float(y), C.float(u), C.float(v))
+	return (*allegro.Color)(unsafe.Pointer(&r))
+}
+
+func YuvToRgb(y, u, v float32) (float32, float32, float32) {
+	var r, g, b C.float
+	C.al_color_yuv_to_rgb(C.float(y), C.float(u), C.float(v), &r, &g, &b)
+	return float32(r), float32(g), float32(b)
+}
+
+func RgbToYuv(r, g, b float32) (float32, float32, float32) {
+	var y, u, v C.float
+	C.al_color_rgb_to_yuv(C.float(r), C.float(g), C.float(b), &y, &u, &v)
+	return float32(y), float32(u), float32(v)
 }
--- graphics.go
+++ graphics.go
@@ -9,43 +9,47 @@ import "unsafe"
 /* Colors */
 /**********/
 
-func MapRgb(r uint8, g uint8, b uint8) Color {
-	return (Color)(C.al_map_rgb(C.uchar(r), C.uchar(g), C.uchar(b)))
+func MapRgb(r uint8, g uint8, b uint8) *Color {
+	c := C.al_map_rgb(C.uchar(r), C.uchar(g), C.uchar(b))
+	return (*Color)(unsafe.Pointer(&c))
 }
 
-func MapRgbF(r float32, g float32, b float32) Color {
-	return (Color)(C.al_map_rgb_f(C.float(r), C.float(g), C.float(b)))
+func MapRgbF(r float32, g float32, b float32) *Color {
+	c := C.al_map_rgb_f(C.float(r), C.float(g), C.float(b))
+	return (*Color)(unsafe.Pointer(&c))
 }
 
-func MapRgba(r uint8, g uint8, b uint8, a uint8) Color {
-	return (Color)(C.al_map_rgba(C.uchar(r), C.uchar(g), C.uchar(b), C.uchar(a)))
+func MapRgba(r uint8, g uint8, b uint8, a uint8) *Color {
+	c := C.al_map_rgba(C.uchar(r), C.uchar(g), C.uchar(b), C.uchar(a))
+	return (*Color)(unsafe.Pointer(&c))
 }
 
-func MapRgbaF(r float32, g float32, b float32, a float32) Color {
-	return (Color)(C.al_map_rgba_f(C.float(r), C.float(g), C.float(b), C.float(a)))
+func MapRgbaF(r float32, g float32, b float32, a float32) *Color {
+	c := C.al_map_rgba_f(C.float(r), C.float(g), C.float(b), C.float(a))
+	return (*Color)(unsafe.Pointer(&c))
 }
 
-func (c Color) UnmapRgb() (uint8, uint8, uint8) {
+func (c *Color) UnmapRgb() (uint8, uint8, uint8) {
 	var r, g, b C.uchar
-	C.al_unmap_rgb((C.ALLEGRO_COLOR)(c), &r, &g, &b)
+	C.al_unmap_rgb((C.ALLEGRO_COLOR)(*c), &r, &g, &b)
 	return uint8(r), uint8(g), uint8(b)
 }
 
-func (c Color) UnmapRgbF() (float32, float32, float32) {
+func (c *Color) UnmapRgbF() (float32, float32, float32) {
 	var r, g, b C.float
-	C.al_unmap_rgb_f((C.ALLEGRO_COLOR)(c), &r, &g, &b)
+	C.al_unmap_rgb_f((C.ALLEGRO_COLOR)(*c), &r, &g, &b)
 	return float32(r), float32(g), float32(b)
 }
 
-func (c Color) UnmapRgba() (uint8, uint8, uint8, uint8) {
+func (c *Color) UnmapRgba() (uint8, uint8, uint8, uint8) {
 	var r, g, b, a C.uchar
-	C.al_unmap_rgba((C.ALLEGRO_COLOR)(c), &r, &g, &b, &a)
+	C.al_unmap_rgba((C.ALLEGRO_COLOR)(*c), &r, &g, &b, &a)
 	return uint8(r), uint8(g), uint8(b), uint8(a)
 }
 
-func (c Color) UnmapRgbaF() (float32, float32, float32, float32) {
+func (c *Color) UnmapRgbaF() (float32, float32, float32, float32) {
 	var r, g, b, a C.float
-	C.al_unmap_rgba_f((C.ALLEGRO_COLOR)(c), &r, &g, &b, &a)
+	C.al_unmap_rgba_f((C.ALLEGRO_COLOR)(*c), &r, &g, &b, &a)
 	return float32(r), float32(g), float32(b), float32(a)
 }
 
@@ -166,8 +170,8 @@ func (b *Bitmap) GetParent() *Bitmap {
 /**********************/
 /* Drawing Operations */
 /**********************/
-func ClearToColor(c Color) {
-	C.al_clear_to_color((C.ALLEGRO_COLOR)(c))
+func ClearToColor(c *Color) {
+	C.al_clear_to_color((C.ALLEGRO_COLOR)(*c))
 }
 
 func ClearDepthBuffer(z float32) {
@@ -178,48 +182,48 @@ func (b *Bitmap) Draw(dx, dy float32, flags int32) {
 	C.al_draw_bitmap((*C.ALLEGRO_BITMAP)(unsafe.Pointer(b)), C.float(dx), C.float(dy), C.int(flags))
 }
 
-func (b *Bitmap) DrawTinted(tint Color, dx, dy float32, flags int32) {
-	C.al_draw_tinted_bitmap((*C.ALLEGRO_BITMAP)(unsafe.Pointer(b)), (C.ALLEGRO_COLOR)(tint), C.float(dx), C.float(dy), C.int(flags))
+func (b *Bitmap) DrawTinted(tint *Color, dx, dy float32, flags int32) {
+	C.al_draw_tinted_bitmap((*C.ALLEGRO_BITMAP)(unsafe.Pointer(b)), (C.ALLEGRO_COLOR)(*tint), C.float(dx), C.float(dy), C.int(flags))
 }
 
 func (b *Bitmap) DrawRegion(sx, sy, sw, sh, dx, dy float32, flags int32) {
 	C.al_draw_bitmap_region((*C.ALLEGRO_BITMAP)(unsafe.Pointer(b)), C.float(sx), C.float(sy), C.float(sw), C.float(sh), C.float(dx), C.float(dy), C.int(flags))
 }
 
-func (b *Bitmap) DrawTintedRegion(tint Color, sx, sy, sw, sh, dx, dy float32, flags int32) {
-	C.al_draw_tinted_bitmap_region((*C.ALLEGRO_BITMAP)(unsafe.Pointer(b)), (C.ALLEGRO_COLOR)(tint), C.float(sx), C.float(sy), C.float(sw), C.float(sh), C.float(dx), C.float(dy), C.int(flags))
+func (b *Bitmap) DrawTintedRegion(tint *Color, sx, sy, sw, sh, dx, dy float32, flags int32) {
+	C.al_draw_tinted_bitmap_region((*C.ALLEGRO_BITMAP)(unsafe.Pointer(b)), (C.ALLEGRO_COLOR)(*tint), C.float(sx), C.float(sy), C.float(sw), C.float(sh), C.float(dx), C.float(dy), C.int(flags))
 }
 
-func (b *Bitmap) DrawPixel(x, y float32, c Color) {
-	C.al_draw_pixel(C.float(x), C.float(y), (C.ALLEGRO_COLOR)(c))
+func (b *Bitmap) DrawPixel(x, y float32, c *Color) {
+	C.al_draw_pixel(C.float(x), C.float(y), (C.ALLEGRO_COLOR)(*c))
 }
 
 func (b *Bitmap) DrawRotated(cx, cy, dx, dy, angle float32, flags int32) {
 	C.al_draw_rotated_bitmap((*C.ALLEGRO_BITMAP)(unsafe.Pointer(b)), C.float(cx), C.float(cy), C.float(dx), C.float(dy), C.float(angle), C.int(flags))
 }
 
-func (b *Bitmap) DrawTintedRotated(c Color, cx, cy, dx, dy, angle float32, flags int32) {
-	C.al_draw_tinted_rotated_bitmap((*C.ALLEGRO_BITMAP)(unsafe.Pointer(b)), (C.ALLEGRO_COLOR)(c), C.float(cx), C.float(cy), C.float(dx), C.float(dy), C.float(angle), C.int(flags))
+func (b *Bitmap) DrawTintedRotated(c *Color, cx, cy, dx, dy, angle float32, flags int32) {
+	C.al_draw_tinted_rotated_bitmap((*C.ALLEGRO_BITMAP)(unsafe.Pointer(b)), (C.ALLEGRO_COLOR)(*c), C.float(cx), C.float(cy), C.float(dx), C.float(dy), C.float(angle), C.int(flags))
 }
 
 func (b *Bitmap) DrawScaledRotated(cx, cy, dx, dy, xscale, yscale, angle float32, flags int32) {
 	C.al_draw_scaled_rotated_bitmap((*C.ALLEGRO_BITMAP)(unsafe.Pointer(b)), C.float(cx), C.float(cy), C.float(dx), C.float(dy), C.float(xscale), C.float(yscale), C.float(angle), C.int(flags))
 }
 
-func (b *Bitmap) DrawTintedScaledRotated(c Color, cx, cy, dx, dy, xscale, yscale, angle float32, flags int32) {
-	C.al_draw_tinted_scaled_rotated_bitmap((*C.ALLEGRO_BITMAP)(unsafe.Pointer(b)), (C.ALLEGRO_COLOR)(c), C.float(cx), C.float(cy), C.float(dx), C.float(dy), C.float(xscale), C.float(yscale), C.float(angle), C.int(flags))
+func (b *Bitmap) DrawTintedScaledRotated(c *Color, cx, cy, dx, dy, xscale, yscale, angle float32, flags int32) {
+	C.al_draw_tinted_scaled_rotated_bitmap((*C.ALLEGRO_BITMAP)(unsafe.Pointer(b)), (C.ALLEGRO_COLOR)(*c), C.float(cx), C.float(cy), C.float(dx), C.float(dy), C.float(xscale), C.float(yscale), C.float(angle), C.int(flags))
 }
 
-func (b *Bitmap) DrawTintedScaledRotatedRegion(sx, sy, sw, sh float32, c Color, cx, cy, dx, dy, xscale, yscale, angle float32, flags int) {
-	C.al_draw_tinted_scaled_rotated_bitmap_region((*C.ALLEGRO_BITMAP)(unsafe.Pointer(b)), C.float(sx), C.float(sy), C.float(sw), C.float(sh), (C.ALLEGRO_COLOR)(c), C.float(cx), C.float(cy), C.float(dx), C.float(dy), C.float(xscale), C.float(yscale), C.float(angle), C.int(flags))
+func (b *Bitmap) DrawTintedScaledRotatedRegion(sx, sy, sw, sh float32, c *Color, cx, cy, dx, dy, xscale, yscale, angle float32, flags int) {
+	C.al_draw_tinted_scaled_rotated_bitmap_region((*C.ALLEGRO_BITMAP)(unsafe.Pointer(b)), C.float(sx), C.float(sy), C.float(sw), C.float(sh), (C.ALLEGRO_COLOR)(*c), C.float(cx), C.float(cy), C.float(dx), C.float(dy), C.float(xscale), C.float(yscale), C.float(angle), C.int(flags))
 }
 
 func (b *Bitmap) DrawScaled(sx, sy, sw, sh, dx, dy, dw, dh float32, flags int32) {
 	C.al_draw_scaled_bitmap((*C.ALLEGRO_BITMAP)(unsafe.Pointer(b)), C.float(sx), C.float(sy), C.float(sw), C.float(sh), C.float(dx), C.float(dy), C.float(dw), C.float(dh), C.int(flags))
 }
 
-func (b *Bitmap) DrawTintedScaled(c Color, sx, sy, sw, sh, dx, dy, dw, dh float32, flags int32) {
-	C.al_draw_tinted_scaled_bitmap((*C.ALLEGRO_BITMAP)(unsafe.Pointer(b)), (C.ALLEGRO_COLOR)(c), C.float(sx), C.float(sy), C.float(sw), C.float(sh), C.float(dx), C.float(dy), C.float(dw), C.float(dh), C.int(flags))
+func (b *Bitmap) DrawTintedScaled(c *Color, sx, sy, sw, sh, dx, dy, dw, dh float32, flags int32) {
+	C.al_draw_tinted_scaled_bitmap((*C.ALLEGRO_BITMAP)(unsafe.Pointer(b)), (C.ALLEGRO_COLOR)(*c), C.float(sx), C.float(sy), C.float(sw), C.float(sh), C.float(dx), C.float(dy), C.float(dw), C.float(dh), C.int(flags))
 }
 
 func GetTargetBitmap() *Bitmap {
@@ -230,12 +234,12 @@ func (b *Bitmap) SetTarget() {
 	C.al_set_target_bitmap((*C.ALLEGRO_BITMAP)(unsafe.Pointer(b)))
 }
 
-func PutPixel(x, y int32, c Color) {
-	C.al_put_pixel(C.int(x), C.int(y), (C.ALLEGRO_COLOR)(c))
+func PutPixel(x, y int32, c *Color) {
+	C.al_put_pixel(C.int(x), C.int(y), (C.ALLEGRO_COLOR)(*c))
 }
 
-func PutBlendedPixel(x, y int32, c Color) {
-	C.al_put_blended_pixel(C.int(x), C.int(y), (C.ALLEGRO_COLOR)(c))
+func PutBlendedPixel(x, y int32, c *Color) {
+	C.al_put_blended_pixel(C.int(x), C.int(y), (C.ALLEGRO_COLOR)(*c))
 }
 
 func (d *Display) SetTargetBackbuffer() {
@@ -292,8 +296,8 @@ func ResetClippingRectangle() {
 /* Graphics Utility Functions */
 /******************************/
 
-func (b *Bitmap) ConvertMaskToAlpha(maskColor Color) {
-	C.al_convert_mask_to_alpha((*C.ALLEGRO_BITMAP)(unsafe.Pointer(b)), (C.ALLEGRO_COLOR)(maskColor))
+func (b *Bitmap) ConvertMaskToAlpha(maskColor *Color) {
+	C.al_convert_mask_to_alpha((*C.ALLEGRO_BITMAP)(unsafe.Pointer(b)), (C.ALLEGRO_COLOR)(*maskColor))
 }
 
 /********************/

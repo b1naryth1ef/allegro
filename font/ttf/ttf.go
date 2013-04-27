a2080ef4fa0d62ba3b7b5ee8c4830cb2b44878c8
Cosku Bas <cosku.bas@gmail.com>
2013-04-27 21:02:11 +0300

    TTF fix

--- font/ttf/ttf.go
+++ font/ttf/ttf.go
@@ -6,6 +6,7 @@ package ttf
 import "C"
 
 import (
+	"github.com/tapir/allegro"
 	"github.com/tapir/allegro/font"
 	"unsafe"
 )
@@ -28,26 +29,26 @@ func GetVersion() uint32 {
 	return uint32(C.al_get_allegro_ttf_version())
 }
 
-func LoadFont(filename string, size, flags int32) *Font {
+func LoadFont(filename string, size, flags int32) *font.Font {
 	f := C.CString(filename)
 	defer C.free(unsafe.Pointer(f))
-	return (*Font)(unsafe.Pointer(C.al_load_ttf_font(f, C.int(size), C.int(flags))))
+	return (*font.Font)(unsafe.Pointer(C.al_load_ttf_font(f, C.int(size), C.int(flags))))
 }
 
-func LoadFontF(file *File, filename string, size, flags int32) *Font {
+func LoadFontF(file *allegro.File, filename string, size, flags int32) *font.Font {
 	f := C.CString(filename)
 	defer C.free(unsafe.Pointer(f))
-	return (*Font)(unsafe.Pointer(C.al_load_ttf_font_f((*C.ALLEGRO_FILE)(unsafe.Pointer(file)), f, C.int(size), C.int(flags))))
+	return (*font.Font)(unsafe.Pointer(C.al_load_ttf_font_f((*C.ALLEGRO_FILE)(unsafe.Pointer(file)), f, C.int(size), C.int(flags))))
 }
 
-func LoadFontStretch(filename string, w, h, flags int32) *Font {
+func LoadFontStretch(filename string, w, h, flags int32) *font.Font {
 	f := C.CString(filename)
 	defer C.free(unsafe.Pointer(f))
-	return (*Font)(unsafe.Pointer(C.al_load_ttf_font_stretch(f, C.int(w), C.int(h), C.int(flags))))
+	return (*font.Font)(unsafe.Pointer(C.al_load_ttf_font_stretch(f, C.int(w), C.int(h), C.int(flags))))
 }
 
-func LoadFontStretchF(file *File, filename string, w, h, flags int32) *Font {
+func LoadFontStretchF(file *allegro.File, filename string, w, h, flags int32) *font.Font {
 	f := C.CString(filename)
 	defer C.free(unsafe.Pointer(f))
-	return (*Font)(unsafe.Pointer(C.al_load_ttf_font_stretch_f((*C.ALLEGRO_FILE)(unsafe.Pointer(file)), f, C.int(w), C.int(h), C.int(flags))))
+	return (*font.Font)(unsafe.Pointer(C.al_load_ttf_font_stretch_f((*C.ALLEGRO_FILE)(unsafe.Pointer(file)), f, C.int(w), C.int(h), C.int(flags))))
 }

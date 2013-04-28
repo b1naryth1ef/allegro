1f52504e90799e4e08ac9652363d436e7ef685bb
Cosku Bas <cosku.bas@gmail.com>
2013-04-28 21:26:09 +0300

    Fixes #7

--- platform_darwin.go
+++
@@ -1,8 +0,0 @@
-package allegro
-
-// #include <allegro5/allegro.h>
-import "C"
-
-func (d *Display) OsxGetWindow() *C.NSWindow {
-	return C.al_osx_get_window((*C.ALLEGRO_DISPLAY)(unsafe.Pointer(d)))
-}
